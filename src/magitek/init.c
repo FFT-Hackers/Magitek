97f4df61bdeefd513a7f865a901135ace6704c92
Nax <max@bacoux.com>
2018-12-06 00:55:19 +0100

    Add TekReplaceFunction

--- src/magitek/init.c
+++ src/magitek/init.c
@@ -1,4 +1,18 @@
+#include <stdint.h>
 #include <windows.h>
+
+void TekReplaceFunction(void* dst, void* newAddr)
+{
+    DWORD oldProtect;
+    uint32_t jmpDelta;
+
+    jmpDelta = (uint32_t)newAddr - ((uint32_t)dst + 5);
+    VirtualProtect(dst, 5, PAGE_READWRITE, &oldProtect);
+    *(uint8_t*)(dst) = 0xe9;
+    *((uint32_t*)((uint8_t*)dst + 1)) = jmpDelta;
+    VirtualProtect(dst, 5, oldProtect, &oldProtect);
+    FlushInstructionCache(GetCurrentProcess(), dst, 5);
+}
 
 __declspec(dllexport) void TekInit()
 {
